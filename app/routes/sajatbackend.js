2ac11ce1363aab653f7ab7b7051a93042bb22d54
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2022-02-28 12:25:39 +0100

    backend

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -65,7 +65,59 @@ module.exports = function(app) {
   })
 
 
-// Adatfelvitel
+
+
+  // Ez alatt van a termék törlés
+
+  app.post('/termektorles', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'szabo_mate_zarodoga'
+    })
+    
+    connection.connect()
+    
+    connection.query('DELETE FROM termekek WHERE termek_id ='+req.body.bevitel1, function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+  
+      res.send(rows)
+    })
+    
+    connection.end()    
+  
+  })
+
+
+  app.get('/termekek_lekerdezes', (req, res) => {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'szabo_mate_zarodoga'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from termekek', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+  
+      res.send(rows)
+    })
+    
+    connection.end()    
+  
+  })
+
+
+// Adatfelvitel (MÉG NEM MŰKÖDIK!)
 
 app.post('/adatfelvitel', (req, res) => {
   var mysql = require('mysql')
